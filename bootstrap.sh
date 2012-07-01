386d1acf8faea1cd74e7cbe5c0070b7e8741dfe5
Bernd Jungblut <Bernd.Jungblut@googlemail.com>
2012-07-01 15:00:03 +0200

    init

---
+++ bootstrap.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+
+CUNIT_URL="http://sourceforge.net/projects/cunit/files/latest/download"
+CUNIT_PATH=$(pwd)/install
+CUNIT_PACKAGE_NAME=CUnit.tar.bz2
+
+if [ ! -d $CUNIT_PATH/include ]; then
+    echo "URL=$CUNIT_URL"
+    wget -O $CUNIT_PACKAGE_NAME $CUNIT_URL
+    if [ $? -ne 0 ]; then
+	echo "ERROR: $CUNIT_URL download failed"
+	exit 1
+    fi
+    tar xjvf $CUNIT_PACKAGE_NAME
+    pushd CUnit*
+    ./configure --prefix=$CUNIT_PATH
+    if [ $? -ne 0 ]; then 
+	exit 1
+    fi
+    make install
+    popd
+    rm -rf CUnit*
+fi
+
+C_INCLUDE_PATH=$CUNIT_PATH/include gcc example.c -Linstall/lib -Wl,-R/home/bernd/unittest/install/lib -lcunit -o example
+
+./example
